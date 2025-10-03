57eb0f05c7476d08cfd911260cc0c81bba6118c1
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2025-10-03 05:31:03 +0330

    feat: expand and refactor calendar constants
    Added gregorianMonths, weekNames, and weekCodes maps to support both Jalali and Gregorian calendars. Introduced customWeekCodes method for dynamic week code mapping based on the first day of the week. Fixed typos in month names and updated existing maps to be final instead of const.

--- lib/core/utils/constants/app_constants.dart
+++ lib/core/utils/constants/app_constants.dart
@@ -1,7 +1,9 @@
+import 'package:persian_calendar_widget/core/enum/enum.dart';
+
 class AppConstants {
   AppConstants._();
 
-  static const Map<int, String> monthsMap = {
+  static final Map<int, String> jalaliMonths = {
     1: 'فروردین',
     2: 'اردیبهشت',
     3: 'خرداد',
@@ -9,20 +11,79 @@ class AppConstants {
     5: 'مرداد',
     6: 'شهریور',
     7: 'مهر',
-    8: 'آبان',
-    9: 'آذر',
+    8: 'آبان',
+    9: 'آذر',
     10: 'دی',
     11: 'بهمن',
     12: 'اسفند',
   };
-
-  static const Map<int, String> weekMap = {
-    1: 'ش',
-    2: 'ی',
-    3: 'د',
-    4: 'س',
-    5: 'چ',
-    6: 'پ',
-    7: 'ج',
+  static final Map<int, String> gregorianMonths = {
+    1: 'ژانویه',
+    2: 'فوریه',
+    3: 'مارس',
+    4: 'آوریل',
+    5: 'مه',
+    6: 'ژوئن',
+    7: 'ژوئیه',
+    8: 'اوت',
+    9: 'سپتامبر',
+    10: 'اکتبر',
+    11: 'نوامبر',
+    12: 'دسامبر',
   };
+
+  static final Map<int, String> weekNames = {
+    1: 'دوشنبه',
+    2: 'سه‌شنبه',
+    3: 'چهارشنبه',
+    4: 'پنج‌شنبه',
+    5: 'جمعه',
+    6: 'شنبه',
+    7: 'یکشنبه',
+  };
+
+  static final Map<int, String> weekCodes = {
+    1: 'د',
+    2: 'س',
+    3: 'چ',
+    4: 'پ',
+    5: 'ج',
+    6: 'ش',
+    7: 'ی',
+  };
+
+  static Map<int, String> customWeekCodes(FirstDayOfWeek firstDayOfWeek) {
+    if (firstDayOfWeek == FirstDayOfWeek.saturday) {
+      return {
+        1: 'ش',
+        2: 'ی',
+        3: 'د',
+        4: 'س',
+        5: 'چ',
+        6: 'پ',
+        7: 'ج',
+      };
+    }
+    if (firstDayOfWeek == FirstDayOfWeek.sunday) {
+      return {
+        1: 'ی',
+        2: 'د',
+        3: 'س',
+        4: 'چ',
+        5: 'پ',
+        6: 'ج',
+        7: 'ش',
+      };
+    }
+
+    return {
+      1: 'د',
+      2: 'س',
+      3: 'چ',
+      4: 'پ',
+      5: 'ج',
+      6: 'ش',
+      7: 'ی',
+    };
+  }
 }
